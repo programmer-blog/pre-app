d7c2417b28e163faea52b1a9e2cce0244f7b1e7d
Programmer Blog <programmerblog.net@gmail.com>
2025-10-11 10:36:49 +0300

    Update login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,8 @@
 import { useState, type SyntheticEvent } from "react";
-import { Button } from "./Button";
+// import { Button } from "./Button";
 import { StyledInput } from "./StyledComponents/StyledInput";
+import Button from '@mui/material/Button'
+import { TextField } from "@mui/material";
 
 export const LoginForm = () => {
     const [email, setEmail] = useState("");
@@ -12,13 +14,17 @@ export const LoginForm = () => {
     }
     return (
         <form className="flex flex-col gap-4" onSubmit={submitForm}>
+            <TextField id="outlined-basic" label="Email" variant="outlined" />
             <StyledInput
             onChange={(e) => setEmail(e.target.value)}
             type="email" placeholder="Enter your email" />
             <StyledInput
             onChange={(e) => setPassword(e.target.value)}
             type="password" placeholder="Enter your password" />
-            <Button text="Submit" />
+            {/* <Button text="Submit" /> */}
+            <button type="submit" className="h-10 px-6 font-semibold bg-red">Submit</button>
+        <Button variant="outlined">Submit</Button>
+
         </form>
     );
  }
